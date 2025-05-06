30d94e24966c066f5eb087cf4c9764cbe9d7e1e6
j-w-wright <wri24011@byui.edu>
2025-05-06 09:41:47 -0600

    Completed the core requirements.

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -1,9 +1,79 @@
 using System;
+using System.Runtime.CompilerServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        // Core Requirment 1
+        Console.Write("Enter your grade percentage: ");
+        int grade = int.Parse(Console.ReadLine());
+        if (grade >= 90)
+        {
+            Console.WriteLine("You earned an A.");
+        }
+        else if (grade >= 80)
+        {
+            Console.WriteLine("You earned a B.");
+        }
+        else if (grade >= 70)
+        {
+            Console.WriteLine("You earned a C.");
+        }
+        else if (grade >= 60)
+        {
+            Console.WriteLine("You earned a D.");
+        }
+        else
+        {
+            Console.WriteLine("You earned an F.");
+        }
+        // Core Requirement 2
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congratulations! You passed the class.");
+        }
+        else
+        {
+            Console.WriteLine("Sorry, you did not pass the class, but you can!  Please try again.");
+        }
+
+        // Core Requirement 3
+        // I left core requirements 1 and 2 in so that I can see the two different ways of doing this code.
+        Console.Write("Enter your grade percentage: ");
+        int gradePercent = int.Parse(Console.ReadLine());
+        string letter; 
+
+        if (gradePercent >= 90)
+        {
+            letter = "A";
+        }
+        else if (gradePercent >= 80)
+        {
+            letter = "B";
+        }
+        else if (gradePercent >= 70)
+        {
+            letter = "C";
+        }
+        else if (gradePercent >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+        {
+            Console.WriteLine($"You earned a {letter}.");
+        }
+        if (gradePercent >= 70)
+        {
+            Console.WriteLine("Congratulations! You passed the class.");
+        }
+        else
+        {
+            Console.WriteLine("Sorry, you did not pass the class, but you can!  Please try again.");
+        }
     }
 }
