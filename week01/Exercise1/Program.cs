59f0639740cea9aaaae2e84f77ca22acfbb19658
j-w-wright <wri24011@byui.edu>
2025-05-06 08:56:55 -0600

    deleted the placeholder text.

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,7 +4,6 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
         Console.WriteLine("What is your first name?" );
         string firstName = Console.ReadLine();
         Console.WriteLine("What is your last name?" );
