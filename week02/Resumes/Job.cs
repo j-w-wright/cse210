be5690b8bb761752a79c93f6551fd8978bb91f9d
j-w-wright <wri24011@byui.edu>
2025-05-10 21:16:48 -0600

    added display job details to the code

---
+++ week02/Resumes/Job.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Security.AccessControl;
+
+public class Job
+{
+    public string _JobTitle;
+    public string _Company;
+    public int _StartYear;
+    public int _EndYear;
+    public string _DisplayJobDetails;
+
+}
