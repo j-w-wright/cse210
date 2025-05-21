b0b657c527fbb4f10e5f296ea35b2249c5c1bd6d
j-w-wright <wri24011@byui.edu>
2025-05-20 20:22:45 -0600

    fractions program

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -5,5 +5,17 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Fractions Project.");
+
+        Fraction fraction1 = new Fraction();
+        Console.WriteLine($"Fraction 1: {fraction1.GetFractionString()}");
+        Console.WriteLine($"Decimal value of Fraction 1: {fraction1.GetDecimalValue()}");
+
+        Fraction fraction2 = new Fraction(3);
+        Console.WriteLine($"Fraction 2: {fraction2.GetFractionString()}");
+        Console.WriteLine($"Decimal value of Fraction 2: {fraction2.GetDecimalValue()}");
+
+        Fraction fraction3 = new Fraction(8, 23);
+        Console.WriteLine($"Fraction 3: {fraction3.GetFractionString()}");
+        Console.WriteLine($"Decimal value of Fraction 3: {fraction3.GetDecimalValue()}");
     }
 }
