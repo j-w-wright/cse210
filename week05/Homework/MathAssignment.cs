0fd78bf7112ceabdb34bb6187c65a237e2ff7a17
j-w-wright <wri24011@byui.edu>
2025-05-31 23:57:17 -0600

    layout of math assignment.

---
+++ week05/Homework/MathAssignment.cs
@@ -0,0 +1,29 @@
+using System;
+
+public class MathAssignment : Assignment
+{
+    private string _textbookSection;
+    private string _problems;
+
+    public string GetTextbookSection()
+    {
+        return _textbookSection;
+    }
+    public void SetTextbookSection(string textbookSection)
+    {
+        _textbookSection = textbookSection;
+    }
+
+    public string GetProblems()
+    {
+        return _problems;
+    }
+    public void SetProblems(string problems)
+    {
+        _problems = problems;
+    }
+    public string GetHomeworkList()
+    {
+        return $"{_textbookSection} {_problems}";
+    }
+}
--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -5,5 +5,16 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Homework Project.");
+
+        // Create an instance of Assignment
+        Assignment assignment = new Assignment();
+        // Create an instance of MathAssignment
+        MathAssignment mathAssignment = new MathAssignment();
+        mathAssignment.SetStudentName("Jared Wright");
+        mathAssignment.SetTopic("Geometry");
+        mathAssignment.SetTextbookSection("Section 5.2");
+        mathAssignment.SetProblems("Problems 1-10");
+        Console.WriteLine(mathAssignment.GetSummary());
+        Console.WriteLine(mathAssignment.GetHomeworkList());
     }
 }
